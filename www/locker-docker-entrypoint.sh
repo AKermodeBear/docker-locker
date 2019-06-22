708601ed7b4d52b552e64bec19d0a78024ed7828
bear <kermodebear@gmail.com>
2019-06-22 03:52:30 +0000

    Trimming the password because it's just easier that way.

--- www/locker-docker-entrypoint.sh
+++ www/locker-docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Updating locker configuration file."
-php -r '$config = json_decode(file_get_contents("/var/www/config/locker.json"), true); $config["mysql"]["server"] = "db"; $config["mysql"]["password"] = file_get_contents("/var/run/secrets/db.user.pass"); file_put_contents("/var/www/config/locker.json", json_encode($config));'
+php -r '$config = json_decode(file_get_contents("/var/www/config/locker.json"), true); $config["mysql"]["server"] = "db"; $config["mysql"]["password"] = trim(file_get_contents("/var/run/secrets/db.user.pass")); file_put_contents("/var/www/config/locker.json", json_encode($config));'
 # sed -i "s/\"password\":.*$/\"password\": \"$(cat /var/run/secrets/db.user.pass)\",/g" /var/www/config/locker.json
 
 echo "Starting standard php:7.2-apache entry point."
